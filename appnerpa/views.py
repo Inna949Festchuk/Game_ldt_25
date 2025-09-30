8ee8d23fab54d333d3175210d22e3007e5e561d9
Inna949Festchuk <innabori@mail.ru>
2025-09-30 05:52:55 +0200

    v0.0.8

--- appnerpa/views.py
+++ appnerpa/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from django.views.decorators.csrf import csrf_exempt
+
+@csrf_exempt
 def one_c_handler(request):
     return HttpResponse("Request handled successfully.")
 
--- nerpa/settings.py
+++ nerpa/settings.py
@@ -135,3 +135,7 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# CSRF_TRUSTED_ORIGINS = [
+#     'http://localhost:8000',
+# ]
